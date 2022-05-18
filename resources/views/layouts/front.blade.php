1494be20a103956769168426fd72e9292540839d
fathur rizky <61815491+rzkyfathur@users.noreply.github.com>
2022-05-18 10:06:00 +0700

    slicing layouts app, front

---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale())}}">
+<head>
+
+    @include('includes.Landing.meta')
+
+    <title>@yield('title') | Serve</title>
+
+    @stack('before-style')
+
+    @include('includes.Landing.style')
+
+    @stack('after-style')
+</head>
+<body class="antialiased">
+    <div class="relative">
+
+        @include('includes.Landing.header')
+                @yield('content')
+
+        @include('includes.landing.footer')
+
+        @stack('before-script')
+
+        @include('includes.landing.script')
+
+        @stack('after-script')
+
+        {{-- modals --}}
+        @include('components.modal.login')
+        @include('components.modal.register')
+        @include('components.modal.register-success')
+
+    </div>
+</body>
+</html>
