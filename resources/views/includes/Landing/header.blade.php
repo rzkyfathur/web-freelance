e05fa59ae10c77a273bbf2b394dadb0cf072b87e
fathur rizky <61815491+rzkyfathur@users.noreply.github.com>
2022-06-27 21:52:27 +0700

    update validasi image

--- resources/views/includes/Landing/header.blade.php
+++ resources/views/includes/Landing/header.blade.php
@@ -65,7 +65,7 @@
 
                         Halo, {{ Auth::user()->name; }}
 
-                        <img class="inline ml-3 h-12 w-12 rounded-full" src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80" alt="">
+                        <img class="inline ml-3 h-12 w-12 rounded-full" src="{{ url(Storage::url(auth()->user()->detail_user()->first()->photo))}}" alt="">
 
                         <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                     </button>
